0689abf63a149177eb0390067432f0d21ebc7098
vishwas saini <105575432+vishwas213@users.noreply.github.com>
2025-07-25 00:08:45 +0530

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+const Bot = new Discord.Client();
+global.Bot = Bot
+
+/******************************************************************************************
+               Configuration of the Bot
+******************************************************************************************/
+exports.prefix = "&"; //The Prefix
+
+exports.owners = [""];//You're ID
+
+exports.token = "";// Your Bot's Token goes here
+
+exports.activity = "" // Set Activity
